eadc1bd637f74d6071c3bab41e7abf2950b436cf
mingthor <gaomingduke@gmail.com>
2017-06-20 22:13:37 -0700

    add materials design styles draw effects to web app

--- web/functions/index.js
+++ web/functions/index.js
@@ -15,8 +15,10 @@
  */
 
 // TODO(DEVELOPER): Import the Cloud Functions for Firebase and the Firebase Admin modules here.
-
+const functions = require('firebase-functions');
 // TODO(DEVELOPER): Write the addWelcomeMessages Function here.
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
 // TODO(DEVELOPER): Write the blurOffensiveImages Function here.
 
